56807e542a8ca06bc4234a7617b27d018918b332
heckmartin <martinheck99@outlook.com>
2021-07-14 21:19:38 -0300

    salvando em csv

--- ps_birdie/ps_birdie/spiders/birdie_spider.py
+++ ps_birdie/ps_birdie/spiders/birdie_spider.py
@@ -1,7 +1,7 @@
 import json
 import scrapy
 import pandas as pd
- 
+import ipdb
 class BirdieSpider(scrapy.Spider):
     name = 'ExtraSpider'
     base_url = 'https://www.extra.com.br/api/catalogo-ssr/products/?Filtro=c1_c2&PaginaAtual=%d'
@@ -40,23 +40,19 @@ class BirdieSpider(scrapy.Spider):
             json_data = json.loads(response.text)
             if json_data['review']['userReviews']:
                 for review in json_data['review']['userReviews']:
-                    print(review['text'])
-                    print(self.Data.loc[(self.Data['id']) == self.next_product,['reviews']])
-                    print(type(self.Data.loc[(self.Data['id']) == self.next_product,['reviews']]))
-                    print(self.Data.loc[(self.Data['id']) == self.next_product]['reviews'])
-                    print(type(self.Data.loc[(self.Data['id']) == self.next_product]['reviews']))
-                    self.Data.loc[(self.Data['id']) == self.next_product]['reviews'].append(review['text'])
+                    self.Data.loc[self.Data[self.Data['id'] == self.next_product].index[0],'reviews'].append(review['text'])
                 self.pag_rev_atual += 1
                 next_page = 'https://pdp-api.extra.com.br/api/v2/reviews/product/' +str(self.next_product)+'/source/EX?page=' +str(self.pag_rev_atual)+'&size=50'
                 yield scrapy.Request(url = next_page, callback=self.parse)
 
             else:
                 if self.produtos_ids: 
-                    self.pag_rev_atual = 0
+                    self.pag_rev_atual = 0  
                     self.next_product = self.produtos_ids.pop()
                     next_page = 'https://pdp-api.extra.com.br/api/v2/reviews/product/' +str(self.next_product)+'/source/EX?page=' +str(self.pag_rev_atual)+'&size=50'
                     yield scrapy.Request(url = next_page, callback=self.parse)
                 else:
+                    print('salvando')
                     print(self.crawled)
                     self.Data.to_csv('Birdie_PS.csv',index = False)
 
